ff2cdfaa533e799a94fc6e52a7b54b7d2f3e06cb
YairGmP <2302133@utrivieramaya.edu.mx>
2025-04-01 12:39:35 -0500

    Conexion de base de datos

--- src/BD/mysql.js
+++ src/BD/mysql.js
@@ -1,8 +1,35 @@
+const mysql = require('mysql2')
 const config = require('../config')
 
 const dbconfig = {
-    host:config.mysql.host
+    host:config.mysql.host,
+    user:config.mysql.user,
+    password:config.mysql.password,
+    database:config.mysql.database
+
 }
+
+function conMysql(){
+    const conexion = mysql.createConnection(dbconfig);
+    conexion.connect((err)=>{
+        if(err){
+            console.log('[BD err]', err)
+            setTimeout(conMysql, 200);
+        }else{
+            console.log('BD conectada')
+        }
+    })
+    conexion.on('error', err =>{
+        console.log('[BD error]', err);
+        if(err.code === 'PROTOCOL_CONNECTRION_LOST'){
+            conMysql()
+        }else{
+            throw err;
+        }
+    })
+}
+
+conMysql()
 
 function todos(tabla){
     const mensaje = 'Soy consulta todos y esta es la tabla a consultar: ' + tabla
